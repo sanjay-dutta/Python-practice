7e7c7aece00314c0805ab8e9857d9762eac1865a
Sanjay Jyoti Dutta <sanjaydutta012@gmail.com>
2022-09-19 23:51:32 +0100

    Create Operator_precedence.py

---
+++ Operator_precedence.py
@@ -0,0 +1,13 @@
+#Operator precedence 
+
+x = 10 + 3 * 2
+print (x)
+
+#Order of operations: ( BODMAS)
+#Exponential (2 ** 2)>multiplication or division>addition or subtraction
+
+x = 10 + 3 * 2 ** 2
+print (x)
+
+x = (10 + 3) * 2 ** 2
+print (x)
