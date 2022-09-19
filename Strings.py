d654cc0b0f3fce9ac9125cf68020b60db15aa340
Sanjay Jyoti Dutta <sanjaydutta012@gmail.com>
2022-09-19 13:20:38 +0100

    Create Strings.py

---
+++ Strings.py
@@ -0,0 +1,26 @@
+course = '''Hi How are you?'''
+print (course)
+
+course = 'Hi How are you?'
+print (course [0])
+
+course = 'Hi How are you?'
+print (course [-1])
+
+course = 'Hi How are you?'
+print (course [0:3])
+
+course = 'Hi How are you?'
+print (course [0:])
+
+course = 'Hi How are you?'
+print (course [1:])
+
+course = 'Hi How are you?'
+print (course [:5])
+
+course = 'Hi How are you?'
+print (course [:])
+
+course = 'Python'
+print (course [1:-1])
