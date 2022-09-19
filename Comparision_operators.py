b89d1d7769f08dff4b8e1c217a97220305e1c0a3
Sanjay Jyoti Dutta <sanjaydutta012@gmail.com>
2022-09-19 23:59:16 +0100

    Create Comparision_operators.py

---
+++ Comparision_operators.py
@@ -0,0 +1,71 @@
+#Comparison operators:
+
+temperature = 30
+
+if temperature > 20:
+	print ("It's a hot day")
+else:
+	print ("It's a cold day")
+
+temperature = 30
+
+if temperature > 20:
+	print ("It's a hot day")
+else:
+	print ("It's a cold day")
+
+
+temperature = 30
+
+if temperature == 20:
+	print ("It's a hot day")
+else:
+	print ("It's a cold day")
+
+
+
+
+
+temperature = 21
+
+if temperature == 20:
+	print ("It's a hot day")
+else:
+	print ("It's a cold day")
+
+
+temperature = 21
+
+if temperature != 20:
+	print ("It's a hot day")
+else:
+	print ("It's a cold day")
+
+
+name = "S"
+
+if len(name)<3:
+
+   print ("Name must be atleast 3 characters")
+
+elif len(name)>50:
+
+	print (" Name must be maximum of 50 characters.")
+
+else:
+	print ("Name looks good.")
+
+
+
+name = "Sanjay"
+
+if len(name)<3:
+
+   print ("Name must be at least 3 characters")
+
+elif len(name)>50:
+
+	print (" Name must be maximum of 50 characters.")
+
+else:
+	print ("Name looks good.")
