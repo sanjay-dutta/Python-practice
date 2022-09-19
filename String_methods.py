21513ee42ce4cf91aad04c00aef20811929de1c6
Sanjay Jyoti Dutta <sanjaydutta012@gmail.com>
2022-09-19 13:27:13 +0100

    Create String_methods.py

---
+++ String_methods.py
@@ -0,0 +1,22 @@
+course = 'Python for beginners'
+print (len(course))
+
+course = 'Python for beginners'
+print (course.upper()) #a function is related to a variable is called method
+
+course = 'Python for beginners'
+print (course.upper())
+print (course.find('P'))
+
+course = 'Python for beginners'
+print (course.find('beginners'))
+print (course.find('B'))
+
+course = 'Python for beginners'
+print (course.replace('beginners', 'Absolute beginners'))
+
+course = 'Python for beginners'
+print (course.replace('P', 'A'))
+
+course = 'Python for beginners'
+print ('Python' in course) # using in operator
