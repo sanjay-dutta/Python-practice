c14e0d57904281449d183b3e794af462fcf2bd75
Sanjay Jyoti Dutta <sanjaydutta012@gmail.com>
2022-09-19 13:27:20 +0100

    Create Formatted_strings.py

---
+++ Formatted_strings.py
@@ -0,0 +1,10 @@
+First = 'Sanjay'
+Last = 'Dutta'
+
+message = First + ' [' + Last  + ']  is a coder'
+print (message)
+
+First = 'Sanjay'
+Last = 'Dutta'
+msg =  f'{First} [{Last}] is a coder' #formated string
+print (msg)
