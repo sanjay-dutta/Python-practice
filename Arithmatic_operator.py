9b070dc1ef4a29389e46e584ad3b108337f12722
Sanjay Jyoti Dutta <sanjaydutta012@gmail.com>
2022-09-19 23:44:09 +0100

    Create Untitled-1.py

---
+++ Arithmatic_operator.py
@@ -0,0 +1,16 @@
+#Arithmetic operator
+
+print(10+3)
+print(10/3)
+print(10//3)
+print(10 % 3)
+print(10 ** 3)
+
+
+x = 3
+x = x + 3
+print (x)
+
+x = 3
+x += 3 # augmented or enhanced assignment operator
+print (x)
