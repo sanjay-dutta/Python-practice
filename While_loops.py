2fa328a1064f8a9a58faa8df05a6af517a3bf7d7
Sanjay Jyoti Dutta <sanjaydutta012@gmail.com>
2022-09-20 00:02:09 +0100

    Create While_loops.py

---
+++ While_loops.py
@@ -0,0 +1,23 @@
+#While loops
+
+i = 1
+while i<=5:
+    print(i)
+    i = i + 1
+print("done")
+
+
+
+i = 1
+while i<=5:
+    i = i + 1
+    print(i)
+    
+print("done")
+
+
+i = 1
+while i<=5:
+    print('*' * i)
+    i = i + 1
+print("done")
