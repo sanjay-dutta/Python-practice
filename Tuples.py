05c36a84f08097228756b9a22384c0572b3fd4fc
Sanjay Jyoti Dutta <sanjaydutta012@gmail.com>
2022-09-23 09:39:52 +0100

    Create Tuples.py

---
+++ Tuples.py
@@ -0,0 +1,13 @@
+# Tuples
+numbers = (1, 2, 3)
+print(numbers[0])
+
+# Tuple object does not support item assignment
+#numbers = (1, 2, 3)
+#numbers[0] = 10
+# print(numbers)
+
+#unpacking 
+coordinates = (1, 2, 3)
+x, y, z = coordinates
+print(x)
