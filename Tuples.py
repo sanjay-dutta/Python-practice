a4f0c43a89688d263b196788b82bf354cfa7e541
Sanjay Jyoti Dutta <sanjaydutta012@gmail.com>
2022-09-23 10:57:58 +0100

    dictionaries

--- Tuples.py
+++ Tuples.py
@@ -3,9 +3,9 @@ numbers = (1, 2, 3)
 print(numbers[0])
 
 # Tuple object does not support item assignment
-#numbers = (1, 2, 3)
-#numbers[0] = 10
-# print(numbers)
+numbers = (1, 2, 3)
+numbers[0] = 10
+print(numbers)
 
 #unpacking 
 coordinates = (1, 2, 3)
