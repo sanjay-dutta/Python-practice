802d1bc8b776a3c2183036d1febcc7296bcd4baf
Sanjay Jyoti Dutta <sanjaydutta012@gmail.com>
2022-09-26 14:54:08 +0100

    Create Paramters.py

---
+++ Paramters.py
@@ -0,0 +1,10 @@
+# Add parameters to the function
+# Here name is the parameter
+def greet_user(name):
+    print(f'Hi {name}!')
+    print("Welcome aboard")
+
+	
+print("Start")
+greet_user("Sanjay")
+print("Finish")
