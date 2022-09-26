7c5fa970950dca084e045ca16a31e6e36cae6d1d
Sanjay Jyoti Dutta <sanjaydutta012@gmail.com>
2022-09-26 20:57:58 +0100

    Update Try_excep.py

--- Try_excep.py
+++ Try_excep.py
@@ -1,3 +1,4 @@
+#The try-except block can handle exceptions. This prevents abrupt exits of the program on error
 #The try block lets you test a block of code for errors.
 #The except block lets you handle the error.
 try: 
