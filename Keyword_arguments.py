1fa9db59a8a5a081d6eaeb17d55aa03cc1fe773f
Sanjay Jyoti Dutta <sanjaydutta012@gmail.com>
2022-09-26 16:31:47 +0100

    arguments

---
+++ Keyword_arguments.py
@@ -0,0 +1,9 @@
+# Keyword arguments
+def greet_user(first_name, last_name):
+    print(f'Hi {first_name} {last_name}!')
+    print("Welcome aboard")
+
+	
+print("Start")
+greet_user(last_name="Dutta", first_name= "Sanjay") # using keyword arguments
+print("Finish")
---
+++ multiple_parameters.py
@@ -0,0 +1,9 @@
+# Add multiple parameters to the function
+def greet_user(first_name, last_name):
+    print(f'Hi {first_name} {last_name}!')
+    print("Welcome aboard")
+
+	
+print("Start")
+greet_user("Sanjay", "Dutta")
+print("Finish")
