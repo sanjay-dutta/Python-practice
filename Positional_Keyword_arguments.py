3157c0c3cc4e5862b872ab9aa5fe3c60d6fb1d55
Sanjay Jyoti Dutta <sanjaydutta012@gmail.com>
2022-09-26 16:46:52 +0100

    Create Positional_Keyword_arguments.py

---
+++ Positional_Keyword_arguments.py
@@ -0,0 +1,9 @@
+# Keyword arguments
+def greet_user(first_name, last_name):
+    print(f'Hi {first_name} {last_name}!')
+    print("Welcome aboard")
+
+	
+print("Start")
+greet_user("Sanjay", last_name="Dutta") # Positional arguments and keyword arguments together
+print("Finish")
