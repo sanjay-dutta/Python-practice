7e32df26eb2ba261235661bcfcbed541e2576685
Sanjay Jyoti Dutta <sanjaydutta012@gmail.com>
2022-10-03 12:10:26 +0100

    push

---
+++ Calendar.py
@@ -0,0 +1,4 @@
+import calendar
+year = 2022
+month = 10
+print(calendar.month(year, month))
--- Class_object concept.py
+++ Class_object concept.py
@@ -5,8 +5,8 @@ class Robot:
         print("My name is " + self.name)
 
 
-r1 = Robot()
-r1.name = "Tom"
+r1 = Robot() # object
+r1.name = "Tom" # Attributes
 r1.color = "Red"
 r1.weight = 30
 r1.introduce_self()
