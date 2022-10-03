78b9a380f913ffecf35ddfbf6d7edd232acfe472
Sanjay Jyoti Dutta <sanjaydutta012@gmail.com>
2022-10-03 19:07:06 +0100

    Create Sample class with init method.py

---
+++ Sample class with init method.py
@@ -0,0 +1,14 @@
+# Sample class with init method
+class Person:
+
+	# init method or constructor
+	def __init__(self, name):
+		self.name = name
+
+	# Sample Method
+	def say_hi(self):
+		print('Hello, my name is', self.name)
+
+
+p = Person('Sanjay')
+p.say_hi()
