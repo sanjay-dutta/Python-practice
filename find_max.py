ea2ece0e1898035f3a30e81ecb5076f0aa680cd3
Sanjay Jyoti Dutta <sanjaydutta012@gmail.com>
2022-10-04 14:43:12 +0100

    push

---
+++ find_max.py
@@ -0,0 +1,6 @@
+# Import the module utils.py which is in the same folder with the file find.py
+
+from utils import find_max
+numbers = [10, 3, 8, 4, 6]
+max = find_max(numbers)
+print(max)
---
+++ utils.py
@@ -0,0 +1,9 @@
+# create a module
+
+def find_max(numbers):
+    max = numbers [0]
+    for number in numbers:
+        if number > max:
+            max = number
+        return max
+
